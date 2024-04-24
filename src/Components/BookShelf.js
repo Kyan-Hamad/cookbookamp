cc20eea84eb1f52d775e002611e27a19ba7a57f7
Kyan Hamad <kyan.hamad@baruchmail.cuny.edu>
2024-04-24 16:48:09 -0400

    Reverted to previous method of displaying books

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Book from './Book';
 import '../Styles/BookShelf.css';
-import { listBooks } from '../graphql/queries';
-import { getUrl } from 'aws-amplify/storage';
-import { generateClient } from 'aws-amplify/api';
 
-const client = generateClient();
-
-function BookShelf({ onDelete }) {
-  const [books, setBooks] = useState([]);
-  
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
-  async function fetchBooks() {
-    const apiData = await client.graphql({ query: listBooks });
-    const booksFromAPI = apiData.data.listBooks.items;
-
-    await Promise.all(
-      booksFromAPI.map(async (book) => {
-        if (book.image) {
-          const url = await getUrl({ key: book.image });
-          book.image = url.url;
-        }
-      })
-    );
-
-    setBooks(booksFromAPI);
-  }
-
-
+function BookShelf({ books, onDelete }) {
   return (
     <div className="book-shelf">
       {books.length === 0 ? (
@@ -40,8 +11,8 @@ function BookShelf({ onDelete }) {
         books.map((book) => (
           <div key={book.id} className="book-item">
             <Book
-               title={book.title}
-               imagePath={book.imagePath ? `http://localhost:5000${book.imagePath}` : 'https://kyan-hamad.github.io/RPG-Game/CookBook-Maker-Logo.png'}
+              title={book.title}
+              image={book.image}
             />
             <button onClick={() => onDelete({ id: book.id, image: book.image })}>Delete</button>
           </div>
